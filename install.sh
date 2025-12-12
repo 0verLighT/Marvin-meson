5340d57ed1f284f441390c4b78c9c7cebd0b6ae5
lchamard <161865209+SeigneurLefou@users.noreply.github.com>
2025-12-12 11:13:04 +0100

    Reorder build command in install script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ rm -Rf .marvin
 git clone https://www.github.com/SeigneurLefou/Marvin-the-Paranoid-Assistant .marvin
 # Move to .marvin
 cd .marvin
-# Build the binary
-make build
 # Add .local/bin to the PATH
 export PATH=$PATH:$HOME/.local/bin/
+# Build the binary
+make build
